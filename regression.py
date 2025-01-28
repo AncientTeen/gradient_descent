fb6eaa9e8fd6f8e107e57afacaa295f04e5a89bf
AncientTeen <mr.michuda777@gmail.com>
2025-01-29 01:07:39 +0200

    - made code looks better

--- regression.py
+++ regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 from icecream import ic
 
+
 def linear_regression(x: list[float], y: list[float], params: list[float], iterations: int,
                       learning_rate: float) -> tuple[list[float], tuple[float, float]]:
     a, b = params
@@ -10,11 +11,12 @@ def linear_regression(x: list[float], y: list[float], params: list[float], itera
     k = 0
     while k < iterations:
         # ic(a, b)
-        loss = (1 / n) * sum((y - (a + b * x)) ** 2)
+        pred = a + b * x
+        loss = (1 / n) * sum((y - pred) ** 2)
         loss_array.append(loss)
 
-        d_a = (-2 / n) * sum((y - (a + b * x)))
-        d_b = (-2 / n) *  sum((x * (y - (a + b * x))))
+        d_a = (-2 / n) * sum((y - pred))
+        d_b = (-2 / n) * sum((x * (y - pred)))
 
         a = a - learning_rate * d_a
         b = b - learning_rate * d_b
@@ -22,7 +24,6 @@ def linear_regression(x: list[float], y: list[float], params: list[float], itera
         k += 1
     ic(a, b)
     return loss_array, (a, b)
-
 
 
 def parabolic_regression(x: list[float], y: list[float], params: list[float], iterations: int,
@@ -34,15 +35,13 @@ def parabolic_regression(x: list[float], y: list[float], params: list[float], it
     k = 0
     while k < iterations:
         # ic(a, b, c)
-
-        loss = (1 / n) * sum((y - (a + b * x + c * x ** 2)) ** 2)
+        pred = a + b * x + c * x ** 2
+        loss = (1 / n) * sum((y - pred) ** 2)
         loss_array.append(loss)
 
-        d_a = (-2 / n) * sum((y - (a + b * x + c * x ** 2)))
-        d_b = (-2 / n) * sum((x * (y - (a + b * x + c * x ** 2))))
-        d_c = (-2 / n) * sum((x ** 2 * (y - (a + b * x + c * x ** 2))))
-
-
+        d_a = (-2 / n) * sum((y - pred))
+        d_b = (-2 / n) * sum((x * (y - pred)))
+        d_c = (-2 / n) * sum((x ** 2 * (y - pred)))
 
         a = a - learning_rate * d_a
         b = b - learning_rate * d_b
@@ -54,8 +53,6 @@ def parabolic_regression(x: list[float], y: list[float], params: list[float], it
     return loss_array, (a, b, c)
 
 
-
-
 def sixth_deg_regression(x: list[float], y: list[float], params: list[float], iterations: int,
                          learning_rate: float) -> tuple[list[float], tuple[float, float, float, float, float, float]]:
     a, b, c, d, e, f, g = params
@@ -64,17 +61,17 @@ def sixth_deg_regression(x: list[float], y: list[float], params: list[float], it
 
     k = 0
     while k < iterations:
-
-        loss = (1 / n) * sum((y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6)) ** 2)
+        pred = a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6
+        loss = (1 / n) * sum((y - pred) ** 2)
         loss_array.append(loss)
 
-        d_a = (-2 / n) * sum((y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6)))
-        d_b = (-2 / n) * sum((x * (y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6))))
-        d_c = (-2 / n) * sum((x ** 2 * (y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6))))
-        d_d = (-2 / n) * sum((x ** 3 * (y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6))))
-        d_e = (-2 / n) * sum((x ** 4 * (y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6))))
-        d_f = (-2 / n) * sum((x ** 5 * (y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6))))
-        d_g = (-2 / n) * sum((x ** 6 * (y - (a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4 + f * x ** 5 + g * x ** 6))))
+        d_a = (-2 / n) * sum((y - pred))  # ∂/∂a
+        d_b = (-2 / n) * sum((x * (y - pred)))  # ∂/∂b
+        d_c = (-2 / n) * sum((x ** 2 * (y - pred)))  # ∂/∂c
+        d_d = (-2 / n) * sum((x ** 3 * (y - pred)))  # ∂/∂d
+        d_e = (-2 / n) * sum((x ** 4 * (y - pred)))  # ∂/∂e
+        d_f = (-2 / n) * sum((x ** 5 * (y - pred)))  # ∂/∂f
+        d_g = (-2 / n) * sum((x ** 6 * (y - pred)))  # ∂/∂g
 
         a = a - learning_rate * d_a
         b = b - learning_rate * d_b
@@ -84,8 +81,6 @@ def sixth_deg_regression(x: list[float], y: list[float], params: list[float], it
         f = f - learning_rate * d_f
         g = g - learning_rate * d_g
 
-
         k += 1
-    ic(a, b, c, d, e, f, g)
-    return loss_array, (a, b, c, d, e, f, g)
 
+    return loss_array, (a, b, c, d, e, f, g)
